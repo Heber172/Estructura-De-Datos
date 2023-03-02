d9679bb2de818e3dd925dc3e37be99f3535377a9
Heber172 <110399924+Heber172@users.noreply.github.com>
2023-03-02 14:58:50 +0100

    Add files via upload

---
+++ Hito2/Laboratorios/LAB_2/Math_utils.java
@@ -0,0 +1,51 @@
+package ejercicio;
+
+    public class Math_utils
+    {
+        private String nameAPP;
+        private String version;
+        private int year;
+
+        public Math_utils(String nameAPP, String version, int year)
+        {
+            this.nameAPP = nameAPP;
+            this.version = version;
+            this.year = year;
+        }
+        public void printApp()
+        {
+            System.out.println(this.nameAPP + " - " + this.version + " - " + this.year);
+            System.out.println();
+        }
+        public void numerosNaturales(int limite)
+        {
+            for(int i = 1; i<=limite; i++)
+            {
+                System.out.print( i+" ");
+            }
+            System.out.println("");
+        }
+        public void numerosPares(int limite)
+        {
+            for(int i = 1; i<=limite; i++)
+            {
+                if(i%2 == 0)
+                {
+                    System.out.print(i+ ", ");
+                }
+            }
+            System.out.println("");
+            System.out.println("");
+        }
+        public void getExtensionFromCI(String ci)
+        {
+            System.out.println("Muestra extencion de la cedula");
+            String ext=" ";
+
+            ext = ci.substring(8);
+
+            System.out.println("Extension = "+ ext);
+
+        }
+    }
+
